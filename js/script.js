232ac5157d909021d0cd658d83683a9816a9a22c
Roma <roman.tytiuk99@gmail.com>
2022-08-04 05:01:28 +0300

    four

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		dots:true,
+		arrows: false,
+		infinite: true,
+		slidesToShow:1,
+		autoplay:true,
+		speed:1000,
+		autoplaySpeed:800
+	});
+	console.log('Loh');
+});
+
