7e6b865674553ad7d3f867fa8056fb238b1b8e8d
Roma <roman.tytiuk99@gmail.com>
2022-08-05 18:09:56 +0300

    Update

--- js/script.js
+++ js/script.js
@@ -11,3 +11,10 @@ $(document).ready(function(){
 	console.log('Loh');
 });
 
+
+$(document).ready(function(){
+	$('.header__burger').click(function(event) {
+		$('.header__burger,.header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
